2b666f92ce7679f1003440c5c22a9d6360c861e9
himavarshini28 <himavarshinimahapatruni@gmail.com>
2025-08-28 22:42:37 +0530

    hi

--- server/server.js
+++ server/server.js
@@ -8,16 +8,7 @@ dotenv.config();
 connectDB();
 
 const app=express();
-app.use(cors({
-  origin: [
-    'https://trueword-e4vi.vercel.app',
-    'https://trueword-e4vi-blat34spz-himavarshinis-projects.vercel.app', 
-    'http://localhost:5173' 
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth/', authRoutes);
